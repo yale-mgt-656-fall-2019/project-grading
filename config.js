3d0b266e6796adab523d77023480fef756b2485a
Kyle L. Jensen <kljensen@gmail.com>
2019-10-17 12:19:36 -0400

    Get basic output working

--- config.js
+++ config.js
@@ -2,25 +2,51 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 const yup = require('yup');
 
+// Todo, test for unique slugs. Can't figure this out right
+// now and need to move on with writing code.
+//
+// Add a method to the yup array object so that we
+// can test for uniqueness.
+// function testUnique(message, mapper = (a) => a) {
+//     console.log('woot');
+//     this.test('unique', message, (list) => {
+//         console.log('haha');
+//         console.log(list.map(mapper));
+//         return false;
+//         return list.length === new Set(list.map(mapper)).size;
+//     });
+// }
+// yup.addMethod(yup.array, 'unique', testUnique);
+
 const testSchema = yup.object().shape({
     it: yup.string().required(),
     slug: yup.string().required(),
     desc: yup.string().required(),
+    passed: yup
+        .boolean()
+        .required()
+        .default(false),
 });
 const testGroupSchema = yup.object().shape({
     when: yup.string().required(),
     slug: yup.string().required(),
-    tests: yup.array().of(testSchema),
+    tests: yup
+        .array()
+        .of(testSchema)
+        .required(),
 });
 const configSchema = yup.object().shape({
     name: yup.string().required(),
-    tests: yup.array().of(testGroupSchema),
+    scenarios: yup
+        .array()
+        .of(testGroupSchema)
+        .required(),
 });
 
 function load(filePath) {
     const doc = yaml.safeLoad(fs.readFileSync(filePath, 'utf8'));
-    configSchema.validateSync(doc);
-    return doc;
+    const doc2 = configSchema.validateSync(doc);
+    return doc2;
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const puppeteer = require('puppeteer');
+const nunjucks = require('nunjucks');
 const config = require('./config.js');
+// const { produce } = require('immer');
+
+// Static Width (Plain Regex)
+const wrap = (s, w) => s
+    .replace(/(?![^\n]{1,70}$)([^\n]{1,70})\s/g, '$1\n')
+    .replace(/^/g, '     ')
+    .replace('\n', '\n     ');
+
+async function showOutput(testSuite, url, course, nickname) {
+    let output = '';
+    testSuite.scenarios.forEach((scenario) => {
+        const when = nunjucks.renderString(scenario.when, {
+            testSuite,
+            url,
+            course,
+            nickname,
+        });
+        output += `When ${when}\n`;
+        scenario.tests.forEach((test) => {
+            const contextData = {
+                testSuite,
+                test,
+                url,
+                course,
+                nickname,
+            };
+            const status = test.passed ? '✅' : '❌';
+            const it = nunjucks.renderString(test.it, contextData);
+            const testDesc = wrap(nunjucks.renderString(test.desc, contextData));
+            output += `${status} - it ${it}\n${testDesc}\n`;
+        });
+    });
+    return output;
+}
 
 (async () => {
-    const suite = config.load('./config.yaml');
-    console.log(suite);
+    const testSuite = config.load('./config.yaml');
+    console.log(testSuite);
     const browser = await puppeteer.launch({
         executablePath: '/usr/bin/chromium-browser',
         args: ['--disable-dev-shm-usage', '--no-sandbox'],
@@ -14,5 +49,7 @@ const config = require('./config.js');
     });
     await page.pdf({ path: 'hn.pdf', format: 'A4' });
 
+    const output = await showOutput(testSuite);
+    console.log(output);
     await browser.close();
 })();
